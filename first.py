5eb62aad0d63b90193ae55888481a995659193f3
DumiVlad <56883011+DumiVlad@users.noreply.github.com>
2019-10-22 16:10:26 +0100

    Create first.py

---
+++ first.py
@@ -0,0 +1,6 @@
+echo "# chatbot-B4" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/DumiVlad/chatbot-B4.git
+git push -u origin master
